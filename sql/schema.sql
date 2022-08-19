af5e98f57cf23fa1036614a977526e3903e8205a
Jayad Arshad <jayadusama@hotmail.co.uk>
2022-08-19 19:38:57 +0100

    created 3 tables in schema.sql

---
+++ sql/schema.sql
@@ -0,0 +1,37 @@
+DROP DATABASE IF EXISTS employeesdb;
+CREATE DATABASE employeesdb;
+
+USE employeesdb;
+
+-- table1
+CREATE TABLE department
+(
+    id INT PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+-- table2
+CREATE TABLE role
+(
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    departmentid INT,
+    FOREIGN KEY (departmentid)
+    REFERENCES department(id)
+);
+
+-- table3
+CREATE TABLE employee
+(
+    id INT PRIMARY KEY,
+    firstname VARCHAR(30),
+    lastname VARCHAR(30),
+    roleid INT,
+    FOREIGN KEY (roleid)
+    REFERENCES role(id),
+    managerid INT NULL,
+    FOREIGN KEY (managerid)
+    REFERENCES employee(id) 
+    ON DELETE CASCADE
+); 
